aa8e2ad332827c3219d29795d70a99ad5a56f787
cutecruelty <cutecruelty2008@gmail.com>
2026-09-07 23:41:12 +0500

    prog 2

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,35 @@ function App() {
   const [reposLoading, setReposLoading] = useState(false);
   const [reposError, setReposError] = useState(null);
 
+  useEffect(() => {
+    if (!debouncedInput) {
+      setUsers([]);
+      return;
+    }
+
+    async function fetchUsers() {
+      setUserLoading(true);
+      setUsersError(null);
+      try {
+        const res = await fetch(
+          `https://api.github.com/search/users?q=${debouncedInput}`,
+        );
+        const data = await res.json();
+        setUsers(data.items);
+      } catch (err) {
+        setUsersError(err.message);
+      } finally {
+        setUserLoading(false);
+      }
+    }
+
+    fetchUsers();
+  }, [debouncedInput]);
+
   return (
     <div className="app">
       <h1>GitHub User Search</h1>
-      <input value={rawInput} onChange={e => setRawInput(e.target.value)}/>
-      
+      <input value={rawInput} onChange={(e) => setRawInput(e.target.value)} />
     </div>
   );
 }
